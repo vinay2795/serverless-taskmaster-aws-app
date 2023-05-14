7c19463706f51615c7b4f6b6ba8d6fd4a08c653d
vinay2795 <36640009+vinay2795@users.noreply.github.com>
2023-05-14 12:53:56 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Serverless! Hope you like it",
+        message: "Hello From Serverless! ",
       },
       null,
       2
